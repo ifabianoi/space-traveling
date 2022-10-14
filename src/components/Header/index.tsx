87aa10f2a587f5a77bdd2ade9e4edb435c32055e
ifabianoi <ifabianoi@gmail.com>
2022-10-13 22:18:15 -0300

    finish challenge

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,18 @@
-export default function Header() {
-  // TODO
-}
+/* eslint-disable no-use-before-define */
+import Link from 'next/link';
+import React from 'react';
+import styles from './header.module.scss';
+
+const Header: React.FC = () => {
+  return (
+    <header className={styles.header}>
+      <Link href="/">
+        <a href="/">
+          <img src="/images/Logo.svg" alt="logo" />
+        </a>
+      </Link>
+    </header>
+  );
+};
+
+export default Header;
